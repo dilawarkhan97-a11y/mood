24276c4d13ea9f7ea773b836c1bb260771b242a5
Dilawar <dilawarkhan97@yahoo.com>
2025-11-12 12:19:26 -0600

    Refactor daily tracking and mood prediction logic
    Updated daily productivity tracking and mood prediction features. Added new tasks and improved data handling for weekly and daily tabs.

--- newapp.py
+++ newapp.py
@@ -99,7 +99,7 @@ with tab_daily:
         mood = "Great 😄"
     st.subheader(f"**Mood:** {mood}")
     # create  daily data
-    Days = [400,425]
+    Days = [400,425,200,325,350,350,325]
     weekly_mean= np.mean(Days)
     st.write("Weekly Mean:", weekly_mean)
     df = pd.DataFrame({"Day": Days})
@@ -180,6 +180,7 @@ with tab_predictor:
     st.write("Next mood probabilities:")
     st.write(probs_series.to_frame("probability"))
     st.success(f"Predicted next mood: {predicted}")
+
 
 
 
